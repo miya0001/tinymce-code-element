a4a7a569bcea582e183ff677754046b26711f9e9
miya <miya@wpist.me>
2013-06-30 04:47:27 +0900

    some fix

--- mce_plugins/code-element/editor_plugin.src.js
+++ mce_plugins/code-element/editor_plugin.src.js
@@ -1,60 +1,52 @@
 (function() {
+    tinymce.create('tinymce.plugins.CodeElementPlugin', {
+        init : function(ed, url) {
 
-	tinymce.create('tinymce.plugins.CodeElementPlugin', {
-		init : function(ed, url) {
-
-			// Register commands
-			ed.addCommand('mceCodeElement', function() {
-				ed.execCommand('mceBeginUndoLevel');
-
-				var e = ed.dom.getParent(ed.selection.getNode(), 'CODE');
-				if (e===null){
-					//add code element
-					if ( ed.selection.isCollapsed() ) {
-						ed.execCommand('mceInsertContent', false, " <code>code</code> ");
-					} else {
+            ed.addCommand('mceCodeElement', function() {
+                ed.execCommand('mceBeginUndoLevel');
+                var e = ed.dom.getParent(ed.selection.getNode(), 'CODE');
+                if (e===null){
+                    if ( ed.selection.isCollapsed() ) {
+                        ed.execCommand('mceInsertContent', false, " <code>code</code> ");
+                    } else {
                         var code = jQuery('<code />');
                         code.html(ed.selection.getContent());
-						ed.execCommand(
+                        ed.execCommand(
                             'mceReplaceContent',
                             false,
                             jQuery('<div />').append(code).html()
                         );
-					}
-				} else {
-					//remove code element
-					ed.execCommand('mceRemoveNode', false, e);
-					ed.nodeChanged();
-				}
+                    }
+                } else {
+                    ed.execCommand('mceRemoveNode', false, e);
+                    ed.nodeChanged();
+                }
 
-				ed.execCommand('mceEndUndoLevel');
-			});
+                ed.execCommand('mceEndUndoLevel');
+            });
 
-			// Register buttons
-			ed.addButton('codeElement', {
-				title : 'Insert/edit code',
-				cmd : 'mceCodeElement',
-				image : url + '/img/icon.png'
-			});
+            ed.addButton('codeElement', {
+                title : 'Insert/edit code',
+                cmd : 'mceCodeElement',
+                image : url + '/img/icon.png'
+            });
 
-			//set button to pressed when cursor at code
-			ed.onNodeChange.add(function(ed, cm, n) {
-				cm.setActive('codeElement', n.nodeName == 'CODE');
-			});
-		},
+            ed.onNodeChange.add(function(ed, cm, n) {
+                cm.setActive('codeElement', n.nodeName == 'CODE');
+            });
+        },
 
-		getInfo : function() {
-			return {
-				longname : 'Code Element',
-				author : 'Takayuki Miyauchi',
-				authorurl : 'http://wpist.me/',
-				infourl : '',
-				version : "0.5"
-			};
-		}
-	});
+        getInfo : function() {
+            return {
+                longname : 'Code Element',
+                author : 'Takayuki Miyauchi',
+                authorurl : 'http://wpist.me/',
+                infourl : '',
+                version : "0.5"
+            };
+        }
+    });
 
-	// Register plugin
-	tinymce.PluginManager.add('codeElement', tinymce.plugins.CodeElementPlugin);
+    tinymce.PluginManager.add('codeElement', tinymce.plugins.CodeElementPlugin);
 })();
 
